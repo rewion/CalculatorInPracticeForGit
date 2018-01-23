683c71f6534fb75207ab317639e2ab491a378c9a
Grzegorz Kwasny <grzegorz.kwasny@gmail.com>
2018-01-23 18:33:29 +0100

    CalculatorChange

---
+++ src/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+    public float plus(float a, float b) {
+        return a + b;
+    }
+
+    public float minus(float a, float b) {
+        return a - b;
+    }
+    public float times (float a, float b){
+        return a*b;
+
+    }
+
+    public float by ( float a, float b){
+        return a/b;
+    }
+
+
+}
